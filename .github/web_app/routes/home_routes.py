bd7a263edf51d4654de98f93fb2cd1059af1a1e0
nehasandesh <ns1193@georgetown.edu>
2022-04-27 03:27:04 -0400

    adding html templates

--- .github/web_app/routes/home_routes.py
+++ .github/web_app/routes/home_routes.py
@@ -8,13 +8,14 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
+    return render_template("home.html")
 
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
     return "About Me"
+    return render_template("about.html")
 
 
 
@@ -33,5 +34,6 @@ def hello_world():
 
     message = f"Hello, {name}!"
     return message
+    return render_template("hello.html", message=message)
 
     
