2a09e223cf2c1f21a20c210d81486e78dfda4b77
nehasandesh <ns1193@georgetown.edu>
2022-04-28 02:42:00 -0400

    last updates

--- .github/web_app/__init__.py
+++ .github/web_app/__init__.py
@@ -1,4 +1,22 @@
 
 
 from flask import flask
-from web app.routes.home_routes
+from web app.routes.home_routes import home_routes
+from web app.routes.home_routes import book_routes
+
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(home_routes)
+    app.register_blueprint(book_routes)
+    app.register_blueprint(weather_routes)
+    return app
+
+
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
+    
